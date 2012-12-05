ed4fcf16df64cb11fba5e5aca9609cefb36b0d1f
deemkeen <dimgur@gmail.com>
2012-12-05 13:01:04 +0100

    Tidy code

--- primabylon.js
+++ primabylon.js
@@ -2,95 +2,106 @@ var Messages = new Meteor.Collection("messages");
 
 if (Meteor.isClient) {
 
-  // Yahoo Query Language Wrapper for jQuery
-  $.YQL = function(query, callback) {
-   
-      if (!query || !callback) {
-          throw new Error('$.YQL(): Parameters may be undefined');
-      }
+    // Yahoo Query Language Wrapper for jQuery
+    $.YQL = function(query, callback) {
 
-      var url = "http://query.yahooapis.com/v1/public/yql?q=" + query + "&format=json&env=store://datatables.org/alltableswithkeys&callback=?";
-      $.getJSON(url, callback);
-      
-  };
+        if (!query || !callback) {
+            throw new Error('$.YQL(): Parameters may be undefined');
+        }
 
-  // helper function
-  function addslashes( str ) {
-      return (str+'').replace(/([\\'])/g, "\\$1").replace(/\0/g, "\\0");
-  }
+        var url = "http://query.yahooapis.com/v1/public/yql?q=" + query + "&format=json&env=store://datatables.org/alltableswithkeys&callback=?";
+        $.getJSON(url, callback);
 
-  // helper time format (time object)
-  function timeformat( obj ) {
+    };
 
-    var currentHours = obj.getHours();
-    var currentMinutes = obj.getMinutes();
-    var currentSeconds = obj.getSeconds();
+    // helper function
 
-    currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
-    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
-    return time = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+    function addslashes(str) {
+        return (str + '').replace(/([\\'])/g, "\\$1").replace(/\0/g, "\\0");
+    }
 
-  }
+    // helper time format (time object)
 
-  // global helper: Works for all Templates!
-  Handlebars.registerHelper('anyMessages', function(){ 
-     return Messages.find().count() > 0;
-  });
 
-  ////////////////////////////////////////////////////////////////////////////////////
+    function timeformat(obj) {
 
-  Template.line.MessageList = function() {
-    return Messages.find({}, {sort: {created: 1}});
-  };
+        var currentHours = obj.getHours();
+        var currentMinutes = obj.getMinutes();
+        var currentSeconds = obj.getSeconds();
 
-  Template.line.rendered = function() {
-              $('#mymessage').val('');
-  };
-  
-  Template.hello.events({
-        "keydown #mymessage": function(event){
+        currentHours = (currentHours < 10 ? "0" : "") + currentHours;
+        currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+        currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
 
-          var code = (event.keyCode ? event.keyCode : event.which);
+        return time = currentHours + ":" + currentMinutes + ":" + currentSeconds;
 
-          if(code == 13){
+    }
 
-              var text = $('#mymessage').val();
-              var sl = $('input:text[name=sl]').val();
-              var tl = $('input:text[name=tl]').val();
+    // global helper: Works for all Templates!
+    Handlebars.registerHelper('anyMessages', function() {
+        return Messages.find().count() > 0;
+    });
 
-              if (text == '#remove') { // Debug
-                $('#mymessage').val('');
-                return Messages.remove({});
-              }
+    ////////////////////////////////////////////////////////////////////////////////////
+    Template.line.MessageList = function() {
+        return Messages.find({}, {
+            sort: {
+                created: 1
+            }
+        });
+    };
 
-              var yq = encodeURIComponent("select json.json.json from google.translate where q='" + addslashes(text) + "' and source='" + sl + "' and target='" + tl + "' limit 1");
+    Template.line.rendered = function() {
+        $('#mymessage').val('');
+    };
 
-              $.YQL(yq, function(data) {
-                
-                var post = data.query.results.json.json.json.json;
+    Template.hello.events({
+        "keydown #mymessage": function(event) {
 
-                if (post) {
+            var code = (event.keyCode ? event.keyCode : event.which);
 
-                  var curdate = new Date();
-                  var curtime = timeformat(curdate);
+            if (code == 13) {
 
-                  Messages.insert({text:post, created: curdate, time: curtime});
+                var text = $('#mymessage').val();
+                var sl = $('input:text[name=sl]').val();
+                var tl = $('input:text[name=tl]').val();
 
+                if (text == '#remove') { // Debug
+                    $('#mymessage').val('');
+                    return Messages.remove({});
                 }
 
-              });
-          }
-      }
-  });
+                var yq = encodeURIComponent("select json.json.json from google.translate where q='" + addslashes(text) + "' and source='" + sl + "' and target='" + tl + "' limit 1");
+
+                $.YQL(yq, function(data) {
+
+                    var post = data.query.results.json.json.json.json;
+
+                    if (post) {
+
+                        var curdate = new Date();
+                        var curtime = timeformat(curdate);
+
+                        Messages.insert({
+                            text: post,
+                            created: curdate,
+                            time: curtime
+                        });
+
+                    }
+
+                });
+            }
+        }
+    });
 
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+    Meteor.startup(function() {
 
         Messages.remove({});
 
-  });
+    });
 }
