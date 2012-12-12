c5a556d1497cdc0021f9dca122f490397ce9e7bd
deemkeen <dimgur@gmail.com>
2012-12-12 23:00:28 +0100

    UTC-Time as default

--- primabylon.js
+++ primabylon.js
@@ -12,9 +12,9 @@ function addslashes(str) {
 // helper time format (time object)
 function timeformat(obj) {
 
-    var currentHours = obj.getHours();
-    var currentMinutes = obj.getMinutes();
-    var currentSeconds = obj.getSeconds();
+    var currentHours = obj.getUTCHours();
+    var currentMinutes = obj.getUTCMinutes();
+    var currentSeconds = obj.getUTCSeconds();
 
     currentHours = (currentHours < 10 ? "0": "") + currentHours;
     currentMinutes = (currentMinutes < 10 ? "0": "") + currentMinutes;
